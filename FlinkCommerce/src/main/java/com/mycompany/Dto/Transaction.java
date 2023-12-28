7b5205ee2ac6fc222f7cb478ae2027b56cae1e67
DucAnhNTT <ducanh0285@gmail.com>
2023-12-28 14:24:01 +0700

    Update

--- FlinkCommerce/src/main/java/com/mycompany/Dto/Transaction.java
+++ FlinkCommerce/src/main/java/com/mycompany/Dto/Transaction.java
@@ -4,7 +4,7 @@
  */
 package com.mycompany.Dto;
 
-import java.security.Timestamp;
+import java.sql.Timestamp;
 
 /**
  *
@@ -17,6 +17,7 @@ import java.security.Timestamp;
  * 'transactionDate': '2023-12-19T05:28:25.444784', 
 'paymentMethod': 'debit_card', 'totalAmount': 3007.06}
  */
+
 public class Transaction {
     private String transactionId;
     private String productId;
@@ -29,4 +30,92 @@ public class Transaction {
     private String customerId;
     private Timestamp transactionDate;
     private String paymentMethod;
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public String getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(String transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductCategory() {
+        return productCategory;
+    }
+
+    public void setProductCategory(String productCategory) {
+        this.productCategory = productCategory;
+    }
+
+    public double getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(double productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public Integer getProductQuantity() {
+        return productQuantity;
+    }
+
+    public void setProductQuantity(Integer productQuantity) {
+        this.productQuantity = productQuantity;
+    }
+
+    public String getProductBrand() {
+        return productBrand;
+    }
+
+    public void setProductBrand(String productBrand) {
+        this.productBrand = productBrand;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public Timestamp getTransactionDate() {
+        return transactionDate;
+    }
+
+    public void setTransactionDate(Timestamp transactionDate) {
+        this.transactionDate = transactionDate;
+    }
 }
--- FlinkCommerce/src/main/java/com/mycompany/flinkcommerce/DataStreamJob.java
+++ FlinkCommerce/src/main/java/com/mycompany/flinkcommerce/DataStreamJob.java
@@ -4,9 +4,11 @@ package com.mycompany.flinkcommerce;
 
 import Deserializer.JSONValueDeserializationSchema;
 import com.mycompany.Dto.Transaction;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
+import org.apache.flink.streaming.api.datastream.DataStream;
 
 
 public class DataStreamJob {
@@ -24,7 +26,11 @@ public class DataStreamJob {
                         .setStartingOffsets(OffsetsInitializer.earliest())
                         .setValueOnlyDeserializer(new JSONValueDeserializationSchema())
                         .build();
-
+                
+                DataStream<Transaction> transactionStream = env.fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka source");
+                
+                transactionStream.print();
+                
 		env.execute("Flink Java API Skeleton");
 	}
 }
