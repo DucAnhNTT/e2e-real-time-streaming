8213d27cd76ad0c03fcbfda756ca1e37b1161796
DucAnhNTT <ducanh0285@gmail.com>
2024-01-22 18:51:20 +0700

    Update on docker-compose for Flink

--- FlinkCommerce/src/main/java/com/mycompany/flinkcommerce/DataStreamJob.java
+++ FlinkCommerce/src/main/java/com/mycompany/flinkcommerce/DataStreamJob.java
@@ -5,10 +5,14 @@ package com.mycompany.flinkcommerce;
 import Deserializer.JSONValueDeserializationSchema;
 import com.mycompany.Dto.Transaction;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.restartstrategy.RestartStrategies;
+import org.apache.flink.api.common.time.Time;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
 import org.apache.flink.streaming.api.datastream.DataStream;
+
+import java.util.concurrent.TimeUnit;
 
 
 public class DataStreamJob {
@@ -17,6 +21,10 @@ public class DataStreamJob {
 		// Sets up the execution environment, which is the main entry point
 		// to building Flink applications.
 		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setRestartStrategy(RestartStrategies.fixedDelayRestart(
+                3, // number of restart attempts
+                Time.of(10, TimeUnit.SECONDS) // delay
+        ));
 
                 String topic = "financial_transactions";
                 KafkaSource<Transaction> source = KafkaSource.<Transaction>builder()
